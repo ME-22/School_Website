613833268e1c3e4d7cd15f39016edffc75f4c178
Juho Lammi <juho.a.lammi@gmail.com>
2026-01-29 21:42:14 +0200

    Suurin osa toimii

--- js/loadParts.js
+++ js/loadParts.js
@@ -1,7 +1,38 @@
-fetch("header.html")
-  .then(res => res.text())
-  .then(data => document.querySelector("header").innerHTML = data);
+// Load header and footer; provide inline fallbacks for file:// usage
+function insertHeader(html) {
+  const el = document.querySelector('header');
+  if (el) el.innerHTML = html;
+}
+function insertFooter(html) {
+  const el = document.querySelector('footer');
+  if (el) el.innerHTML = html;
+}
 
-fetch("footer.html")
-  .then(res => res.text())
-  .then(data => document.querySelector("footer").innerHTML = data);
+fetch('header.html')
+  .then(res => { if (!res.ok) throw new Error('Fetch failed'); return res.text(); })
+  .then(data => insertHeader(data))
+  .catch(() => {
+    insertHeader(`
+      <div>
+        <a href="index.html"><img src="./images/Main Logo.png" alt="Main Logo" /></a>
+      </div>
+      <nav>
+        <a href="home.html">Home</a>
+        <a href="info.html">Info</a>
+        <a href="contact.html">Contact</a>
+      </nav>
+    `);
+  });
+
+fetch('footer.html')
+  .then(res => { if (!res.ok) throw new Error('Fetch failed'); return res.text(); })
+  .then(data => insertFooter(data))
+  .catch(() => {
+    insertFooter(`
+      <h3>CONTACT</h3>
+      <p>Dischargedhc@gmail.com</p>
+      <p>+358 40 0425698</p>
+      <p>13100 Hämeenlinna, Finland</p>
+      <img src="./images/Main Logo.png" alt="Main Logo">
+    `);
+  });
